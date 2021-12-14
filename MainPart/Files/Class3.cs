b2e7973c861f291cc64497d91b5004ccecb1e000
Vladimir <lililolo1222@mail.ru>
2021-12-14 21:48:56 +0300

    Add Class3.cs

---
+++ MainPart/Files/Class3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Example1
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public Example1(IDisposable s, ICloneable c, int a, string str) { }
+
+    public int Function1(int d, int e)
+    {
+        return 0;
+    }
+    public void Function2() { }
+}
+
+public class Example2
+{
+    public IEnumerable<int> Interface { get; private set; }
+
+    public void Function1() { }
+    public void Function2() { }
+}
