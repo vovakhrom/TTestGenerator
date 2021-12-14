94331ec85051d998d8fbce96a0d9b7ad6965a301
Vladimir <lililolo1222@mail.ru>
2021-12-14 21:45:33 +0300

    Add Program.cs

---
+++ MainPart/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MainPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var pathToFolder = "D:\\спп 4\\TestGenerator-master\\MainPart\\Files";
+            var pathToGenerated = @"D:\\спп 4\\TestGenerator-master\\GeneratedTests\\GeneratedFiles";
+            
+            if (!Directory.Exists(pathToFolder))
+            {
+                Console.WriteLine($"Couldn't find directory {pathToFolder}");
+                return;
+            }
+            if (!Directory.Exists(pathToGenerated))
+            {
+                Directory.CreateDirectory(pathToGenerated);
+            }
+
+            var allFiles = Directory.GetFiles(pathToFolder);
+
+            var files = from file in allFiles
+                    where file.Substring(file.Length - 3) == ".cs"
+                    select file;
+
+            Task task =  new Pipeline().Generate(files, pathToGenerated);
+            task.Wait();
+            //Thread.Sleep(2000);
+            Console.WriteLine("end.");
+        }
+    }
+}
