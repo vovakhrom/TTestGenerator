3a6b7e57b3dd08a49ffff6dd54c489e888669dec
Vladimir <lililolo1222@mail.ru>
2021-12-14 21:48:13 +0300

    Add Class1.cs

---
+++ MainPart/Files/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainPart.Files
+{
+    public class Class1
+    {
+
+        int t;
+        public Class1(string str)
+        {
+
+        }
+        public int kj()
+        {
+            int a = 0;
+            int b= a++;
+            return b;
+        }
+    }
+}
